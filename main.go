55eb4ca09fd626bcc51cfff9650aa40c907327d2
iamVihanga <vihangarashansilva@gmail.com>
2025-12-25 19:14:24 +0530

    submit: learn about how to define a variable in go and staticly type the data type, and what are the zero values are

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Strings
+	var myString string
+
+	fmt.Println(myString)
+
+	myString = "Welcome to Go!"
+
+	fmt.Println(myString)
+
+	// Integers
+	var myInt int
+
+	fmt.Println(myInt)
+
+	myInt = 10
+
+	fmt.Println(myInt)
+
+	// Booleans
+	var myBool bool
+
+	fmt.Println(myBool)
+
+	myBool = true
+
+	fmt.Println(myBool)
 }
