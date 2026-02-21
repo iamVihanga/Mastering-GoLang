0f1e414fadfe059191b55c0898a2974b0c2a5872
iamVihanga <vihangarashansilva@gmail.com>
2026-02-21 12:28:28 +0530

    lesson 001: primitive data types

---
+++ lesson001-primitive-data-types/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myString string
+	// String's zero value is an empty string ("")
+
+	fmt.Println(myString)
+
+	myString = "This is lesson 001, Primitive Data Types"
+	fmt.Println(myString)
+
+	// ------------------------
+
+	var myInt int
+	// Integer's zero value is 0
+
+	fmt.Println(myInt)
+
+	myInt = 54
+	fmt.Println(myInt)
+
+	// ------------------------
+
+	var myBoolean bool
+	// Boolean's zero value is false
+
+	fmt.Println(myBoolean)
+
+	myBoolean = true
+	fmt.Println(myBoolean)
+}
